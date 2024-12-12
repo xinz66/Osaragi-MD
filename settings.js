ae6aa205e1fca4f2bf8f08571e28556298fcbc73
xinz66 <paisbonjol@gmail.com>
2024-12-12 21:16:25 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,8 +5,8 @@ const chalk = require('chalk')
 const moment = require('moment-timezone')
 
 //========== Setting Owner ==========//
-global.owner = "6289508082845"
-global.premium = ["6289508082845"]
+global.owner = "6289529022545"
+global.premium = ["6289529022545"]
 global.idsaluran = "120363201331652484@newsletter"
 global.namaowner = "Koi"
 global.namabot = "おさらぎ"
